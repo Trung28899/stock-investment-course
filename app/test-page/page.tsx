0d29cd087476c348b7f2a5b10afb5784a4ab6f63
TrungSofistic <ttrinh@sofistic.ai>
2025-02-01 16:25:38 -0500

    playing around with file structure

---
+++ app/test-page/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TestItem from "@/modules/test-module/TestItem";
+
+function TestPage() {
+  return (
+    <div>
+      <TestItem></TestItem>
+      <TestItem></TestItem>
+    </div>
+  );
+}
+
+export default TestPage;
---
+++ modules/test-module/TestItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestItem() {
+  return <div>TestItem</div>;
+}
+
+export default TestItem;
